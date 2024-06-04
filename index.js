0797e07738ad093e43e236d359778fd0f2573d9f
BiploHossainSobuj <bhs.cse@outlook.com>
2024-06-04 21:20:28 +0600

    added user,taskCreator,admin verification api

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const app = express();
 const port = process.env.port || 5000;
 dotenv.config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 // const stripe = require('stripe')(process.env.STRIPE_SECRETE);
 const jwt = require('jsonwebtoken');
 
@@ -19,8 +20,7 @@ app.listen(port, () => {
     console.log(`server is runing on port ${port}`);
 })
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bswbr7l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bswbr7l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -35,7 +35,101 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
+
+        //collections
+        const userCollection = client.db("micro-service").collection('users');
+
+        //token verify
+        const verifyToken = (req, res, next) => {
+            // console.log('inside verify middleware', req.headers.authorization);
+            if (!req.headers.authorization) {
+              return res.status(401).send({ massage: 'forbidden access' });
+            }
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, 'secret', (err, decoded) => {
+              if (err) {
+                return res.status(401).send({ massage: 'forbidden access' });
+              }
+              req.decoded = decoded
+              next();
+            })
+          }
+        
+        // jwt token 
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            // console.log(user,'from jwt');
+            const token = jwt.sign(user, 'secret', { expiresIn: '1h' })
+            res.send({ token });
+        })
+
+        //users related api
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            // console.log(user);
+            const query = { email: user.email };
+            const isExist = await userCollection.findOne(query);
+            if (isExist) {
+                return res.send({ message: "user already exist", insertedId: null });
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+        app.get('/users',async(req,res)=>{
+            const result = await userCollection.find().toArray();
+            // console.log(result);
+            res.send(result);
+        })
+
+        // admin check 
+        app.get('/users/admin/:email', verifyToken, async (req, res) => {
+            // console.log(req.headers);
+            const email = req.params.email;
+            console.log(email);
+            if (email != req.decoded.email) {
+              return res.status(403).send({ massage: 'unauthorized access from admin' });
+            }
+            const query = { email: email }
+            const result = await userCollection.findOne(query);
+            let admin = false;
+            if (result) {
+              admin = result?.role === 'admin'
+            }
+            // console.log(admin);
+            res.send({ admin });
+          })
+        // task Creator check 
+        app.get('/users/taskCreator/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+            if (email != req.decoded.email) {
+              return res.status(403).send({ massage: 'unauthorized access from admin' });
+            }
+            const query = { email: email }
+            const result = await userCollection.findOne(query);
+            let taskCreator = false;
+            if (result) {
+              taskCreator = result?.role === 'taskCreator'
+            }
+            
+            res.send({ taskCreator });
+          })
+        // worker check 
+        app.get('/users/worker/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+            if (email != req.decoded.email) {
+              return res.status(403).send({ massage: 'unauthorized access from admin' });
+            }
+            const query = { email: email }
+            const result = await userCollection.findOne(query);
+            let worker = false;
+            if (result) {
+              worker = result?.role === 'worker'
+            }
+            
+            res.send({ worker });
+          })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
